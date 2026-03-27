d225aeb606bc6afc427f9946238922173cd2296e
1301235340-commits <1301235340@s.tusd1.org>
2026-03-27 22:07:32 +0000

    day 222

--- script.js
+++ script.js
@@ -14,4 +14,48 @@ function start() {
     // 3. add the heading to the page
     document.body.appendChild(heading);
 
+    let userName = prompt("Enter your name");
+    let userLastName = prompt("Enter your lastname");
+    addName(userName, userLastName);
+    addSeparator();
+
+    userName = prompt("Enter your name");
+    userLastName = prompt("Enter your lastname");
+    addName(userName, userLastName);
+    addSeparator();
+
+        userName = prompt("Enter your name");
+    userLastName = prompt("Enter your lastname");
+    addName(userName, userLastName);
+    addSeparator();
+
+        userName = prompt("Enter your name");
+    userLastName = prompt("Enter your lastname");
+    addName(userName, userLastName);
+    addSeparator();
+
+        userName = prompt("Enter your name");
+    userLastName = prompt("Enter your lastname");
+    addName(userName, userLastName);
+    addSeparator();
 }
+
+function addName(newName, lastname) {
+    // Create h2 element
+    let header = document.createElement("h2");
+    
+    // Set text to Format: Lastname, Firstname
+    header.textContent = lastname + ", " + newName;
+    
+    // Add h2 to the page
+    document.body.appendChild(header);
+}
+
+function addSeparator() {
+    // Create the horizontal rule
+    let line = document.createElement("hr");
+    
+    // Add the line to the page
+    document.body.appendChild(line);
+}
+
